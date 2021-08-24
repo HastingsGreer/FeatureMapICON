5160c8c1e30d10d2c6467c8d3cb43fb8506141ed
Thomas Greer <tgreer@biag-gpu0.cs.unc.edu>
2021-08-24 18:31:01 -0400

    format?

--- fmapicon.py
+++ fmapicon.py
@@ -1,4 +1,4 @@
-import footsteps
+import os
 import scipy.ndimage
 import scipy.ndimage.measurements
 import torch
@@ -10,71 +10,95 @@ import matplotlib.pyplot as plt
 import torch.optim as optim
 from torch.autograd import Function
 from torch.nn import Module
+import footsteps
+
+
 def show(x):
     while len(x.shape) > 2:
         x = x[0]
     plt.imshow(x.detach().cpu())
 
+
 def get_dataset(split):
     ds = torch.utils.data.DataLoader(
-        torchvision.datasets.MNIST("./files/",
+        torchvision.datasets.MNIST(
+            "./files/",
             transform=torchvision.transforms.ToTensor(),
             download=True,
-            train=(split == "train")
+            train=(split == "train"),
         ),
-        batch_size=500
+        batch_size=500,
     )
     images = []
     for _, batch in enumerate(ds):
         label = np.array(batch[1])
-        batch_nines = label ==5
+        batch_nines = label == 5
         images.append(np.array(batch[0])[batch_nines])
     images = np.concatenate(images)
 
     ds = torch.utils.data.TensorDataset(torch.Tensor(images))
-    d1, d2 = (torch.utils.data.DataLoader(ds, batch_size=128, shuffle=True, ) for _ in (1,1))
+    d1, d2 = (
+        torch.utils.data.DataLoader(
+            ds,
+            batch_size=128,
+            shuffle=True,
+        )
+        for _ in (1, 1)
+    )
     return d1, d2
+
+
 d1_mnist, d2_mnist = get_dataset("train")
 d1_mnist_test, d2_mnist_test = get_dataset("test")
 
 
 N = 28
 BATCH_SIZE = 32
+
+
 def get_dataset_triangles(split):
-    x, y = np.mgrid[0:1:N * 1j, 0:1:N * 1j]
+    x, y = np.mgrid[0 : 1 : N * 1j, 0 : 1 : N * 1j]
     x = np.reshape(x, (1, N, N))
     y = np.reshape(y, (1, N, N))
-    cx = np.random.random((6000, 1, 1)) * .2 + .4
-    cy = np.random.random((6000, 1, 1)) * .2 + .4
-    r = np.random.random((6000, 1, 1)) * .2 + .2
+    cx = np.random.random((6000, 1, 1)) * 0.2 + 0.4
+    cy = np.random.random((6000, 1, 1)) * 0.2 + 0.4
+    r = np.random.random((6000, 1, 1)) * 0.2 + 0.2
     theta = np.random.random((6000, 1, 1)) * np.pi * 2
-    isTriangle = np.random.random((6000, 1, 1)) > .5
-    
-    isHollow = np.random.random((6000, 1, 1)) > .5
+    isTriangle = np.random.random((6000, 1, 1)) > 0.5
 
+    isHollow = np.random.random((6000, 1, 1)) > 0.5
 
-    triangles = (np.sqrt((x - cx)**2 + (y - cy)**2) 
-    - r * np.cos(np.pi / 3) / np.cos((np.arctan2(x - cx, y - cy) + theta) % (2 * np.pi / 3) - np.pi / 3))
+    triangles = np.sqrt((x - cx) ** 2 + (y - cy) ** 2) - r * np.cos(np.pi / 3) / np.cos(
+        (np.arctan2(x - cx, y - cy) + theta) % (2 * np.pi / 3) - np.pi / 3
+    )
 
     triangles = np.tanh(-40 * triangles)
 
-    circles = np.tanh(-40 * (np.sqrt((x - cx)**2 + (y - cy)**2) - r) )
-    
-    
+    circles = np.tanh(-40 * (np.sqrt((x - cx) ** 2 + (y - cy) ** 2) - r))
+
     images = isTriangle * triangles + (1 - isTriangle) * circles
-    
-    hollow = 1 - images **2
-    
+
+    hollow = 1 - images ** 2
+
     filled = (images + 1) / 2
-    
+
     images = isHollow * hollow + (1 - isHollow) * filled
 
     ds = torch.utils.data.TensorDataset(torch.Tensor(np.expand_dims(images, 1)))
-    d1, d2 = (torch.utils.data.DataLoader(ds, batch_size=BATCH_SIZE, shuffle=True, ) for _ in (1,1))
+    d1, d2 = (
+        torch.utils.data.DataLoader(
+            ds,
+            batch_size=BATCH_SIZE,
+            shuffle=True,
+        )
+        for _ in (1, 1)
+    )
     return d1, d2
+
 
 d1_triangles, d2_triangles = get_dataset_triangles("train")
 d1_triangles_test, d2_triangles_test = get_dataset_triangles("test")
+
 
 class RegisNetNoPad(nn.Module):
     def __init__(self):
@@ -86,26 +110,26 @@ class RegisNetNoPad(nn.Module):
         self.conv5 = nn.Conv2d(41, 10, kernel_size=5)
         self.conv6 = nn.Conv2d(51, 64, kernel_size=5)
 
-    def forward(self, x):   
+    def forward(self, x):
         x = torch.nn.functional.pad(x, [12] * 4)
         x = torch.cat([x[:, :, 2:-2, 2:-2], F.relu(self.conv1(x))], 1)
         x = torch.cat([x[:, :, 2:-2, 2:-2], F.relu(self.conv2(x))], 1)
         x = torch.cat([x[:, :, 2:-2, 2:-2], F.relu(self.conv3(x))], 1)
         x = torch.cat([x[:, :, 2:-2, 2:-2], F.relu(self.conv4(x))], 1)
         x = torch.cat([x[:, :, 2:-2, 2:-2], F.relu(self.conv5(x))], 1)
-        
+
         out = self.conv6(x)
-        
+
         ##normalize
-        #out_norms = torch.sqrt(torch.sum(out**2, 1, keepdim=True))
-        
-        #out = out / (out_norms + .0001)
-        
+        # out_norms = torch.sqrt(torch.sum(out**2, 1, keepdim=True))
+
+        # out = out / (out_norms + .0001)
+
         return out * 10
 
 
 def train(net, d1, d2):
-    optimizer = torch.optim.Adam(net.parameters(), lr=.0001)
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
     net.train()
     net.cuda()
     loss_history = []
@@ -123,27 +147,30 @@ def train(net, d1, d2):
     print("]")
     return loss_history
 
+
 def pass_(A, B, net, optimizer):
-    
+
     if A[0].size()[0] == BATCH_SIZE:
-                image_A = A[0].cuda()
-                image_B = B[0].cuda()
-                optimizer.zero_grad()
-                
-                nA = net(image_A)[::, ::].reshape(-1, BATCH_SIZE, N * N)
-                nB = net(image_B)[::, ::].reshape(-1, BATCH_SIZE, N * N)
+        image_A = A[0].cuda()
+        image_B = B[0].cuda()
+        optimizer.zero_grad()
 
-                cc = torch.einsum("iCN,iCK->iNK", nA, nB)
+        nA = net(image_A)[::, ::].reshape(-1, BATCH_SIZE, N * N)
+        nB = net(image_B)[::, ::].reshape(-1, BATCH_SIZE, N * N)
 
-                cc_A = torch.softmax(cc, axis=1)
-                cc_B = torch.softmax(cc, axis=2)
-                loss = cc_A * cc_B
-                loss = torch.clamp(loss, max=.3)
-                loss = -torch.sum(loss) / BATCH_SIZE / (N * N)
+        cc = torch.einsum("icn,ick->ink", nA, nB)
 
-                loss.backward()
-                optimizer.step()
-                return loss.detach()
+        cc_A = torch.softmax(cc, axis=1)
+        cc_B = torch.softmax(cc, axis=2)
+        loss = cc_A * cc_B
+        loss = torch.clamp(loss, max=0.3)
+        loss = -torch.sum(loss) / BATCH_SIZE / (N * N)
+
+        loss.backward()
+        optimizer.step()
+        return loss.detach()
+
+
 def do_many_visualizations(prefix, A, B, net):
     prefix = footsteps.output_dir + prefix + "/"
     os.mkdir(prefix)
@@ -152,8 +179,8 @@ def do_many_visualizations(prefix, A, B, net):
     show(B)
     plt.subplot(1, 2, 2)
     show(A)
-    plt.savefig(prefix = "A,B.png")
-
+    plt.savefig(prefix + "A,B.png")
+    plt.clf()
     net.cpu()
 
     for i in range(30):
@@ -161,12 +188,12 @@ def do_many_visualizations(prefix, A, B, net):
         plt.xticks([])
         plt.yticks([])
         show(net(A)[0, i])
-        #plt.colorbar()
+        # plt.colorbar()
 
     nA = net(A).reshape(-1, 64, N * N)
     nB = net(B).reshape(-1, 64, N * N)
 
-    cc = torch.einsum("iCN,iCK->iNK", nA, nB)
+    cc = torch.einsum("icn,ick->ink", nA, nB)
 
     cc_A = torch.softmax(cc, axis=1)
     cc_B = torch.softmax(cc, axis=2)
@@ -175,27 +202,34 @@ def do_many_visualizations(prefix, A, B, net):
     show(loss)
     plt.colorbar()
     plt.savefig(prefix + "loss_full.png")
+    plt.clf()
 
     i, j = 10, 12
 
     show(cc.reshape([N] * 4)[i, j])
     plt.colorbar()
-    y, x = scipy.ndimage.measurements.center_of_mass(cc_A.reshape([N] * 4)[:, :, i, j].detach().numpy())
+    y, x = scipy.ndimage.measurements.center_of_mass(
+        cc_A.reshape([N] * 4)[:, :, i, j].detach().numpy()
+    )
 
     plt.scatter(x, y)
 
-    plt.savefig("One_pixel_regis.png")
+    plt.savefig(prefix + "One_pixel_regis.png")
+    plt.clf()
 
     reshaped = cc_A.reshape([N] * 4).detach().numpy()
 
-    grid = np.array([
+    grid = np.array(
         [
-            scipy.ndimage.measurements.center_of_mass(reshaped[i, j].transpose())
-            for i in range(N)]
-        for j in range(N)
-    ])
-    #grid[:, :, 0] = scipy.ndimage.gaussian_filter(grid[:, :, 0], 1)
-    #grid[:, :, 1] = scipy.ndimage.gaussian_filter(grid[:, :, 1], 1)
+            [
+                scipy.ndimage.measurements.center_of_mass(reshaped[i, j].transpose())
+                for i in range(N)
+            ]
+            for j in range(N)
+        ]
+    )
+    # grid[:, :, 0] = scipy.ndimage.gaussian_filter(grid[:, :, 0], 1)
+    # grid[:, :, 1] = scipy.ndimage.gaussian_filter(grid[:, :, 1], 1)
 
     grid = grid[3:-3, 3:-3]
 
@@ -203,16 +237,23 @@ def do_many_visualizations(prefix, A, B, net):
     plt.plot(grid[:, :, 0].transpose(), grid[:, :, 1].transpose())
     plt.ylim(N, 0)
     plt.savefig(prefix + "/grid.png")
+    plt.clf()
     show(B)
     plt.scatter(grid[:, :, 0], grid[:, :, 1], c="red", s=100)
-    plt.scatter(grid[:, :, 0], grid[:, :, 1], c=np.array(A[0, 0, 3:-3, 3:-3]).transpose(), s=90)
+    plt.scatter(
+        grid[:, :, 0], grid[:, :, 1], c=np.array(A[0, 0, 3:-3, 3:-3]).transpose(), s=90
+    )
     plt.ylim(N, 0)
     plt.savefig(prefix + "/dots.png")
+    plt.clf()
 
     show(torch.sum(loss, axis=1).reshape(N, N))
     plt.colorbar()
-    
+
     plt.savefig(prefix + "/loss_image.png")
+    plt.clf()
+
+
 net = RegisNetNoPad()
 
 A = list(d1_triangles)[0][0][:1]
