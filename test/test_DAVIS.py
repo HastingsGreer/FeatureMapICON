04bfd82298cf2f91681ea88d434a9106044ed68c
Thomas Greer <tgreer@biag-gpu0.cs.unc.edu>
2021-10-17 18:34:09 -0400

    halp

--- test/test_DAVIS.py
+++ test/test_DAVIS.py
@@ -4,8 +4,8 @@ import data
 import tensorflow_.fmapicon_utils
 
 class DavisTestCase(unittest.TestCase):
-    def testDoNothing(self):
-        data.DavisEval(tensorflow_.fmapicon_utils.do_nothing_model, "do_nothing")
+#    def testDoNothing(self):
+#        data.DavisEval(tensorflow_.fmapicon_utils.do_nothing_model, "do_nothing")
     def testFMAPICON(self):
         data.DavisEval(tensorflow_.fmapicon_utils.FMAPICON_model, "FMAPICON")
         
