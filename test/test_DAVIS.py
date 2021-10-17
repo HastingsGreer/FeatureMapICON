478d8a51d0af6ab0b72ab7b414e3753a69b6ee22
Hastings Greer <hastings.greer@gmail.com>
2021-10-17 19:02:42 -0400

    repaise

--- test/test_DAVIS.py
+++ test/test_DAVIS.py
@@ -7,5 +7,5 @@ class DavisTestCase(unittest.TestCase):
 #    def testDoNothing(self):
 #        data.DavisEval(tensorflow_.fmapicon_utils.do_nothing_model, "do_nothing")
     def testFMAPICON(self):
-        data.DavisEval(tensorflow_.fmapicon_utils.FMAPICON_model, "FMAPICON")
+        data.DavisEval(tensorflow_.fmapicon_utils.FMAPICON_model("../tensorflow_/results/log_probability_2/"), "FMAPICON")
         
