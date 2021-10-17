7767fafaab932392b5533d6b95f2ddfaaa72b96a
Hastings Greer <hastings.greer@gmail.com>
2021-10-17 13:10:06 -0400

    crimes

--- data/__init__.py
+++ data/__init__.py
@@ -1,7 +1,11 @@
 import footsteps
-
-def DavisEval(model):
+import os
+import glob
+def DavisEval(model, name):
     davis_path = "data_storage/DAVIS/"
-    output_path = footsteps.output_dir
+    output_path = footsteps.output_dir + name + "/"
+
+    for sequence in glob.glob(davis_path + "/*"):
+        print(sequence)
 
 
--- test/test_DAVIS.py
+++ test/test_DAVIS.py
@@ -1,9 +1,9 @@
 import unittest
 import data
 
-import tensorflow.fmapicon_utils
+import tensorflow_.fmapicon_utils
 
 class DavisTestCase(unittest.TestCase):
     def testDoNothing(self):
-        data.DavisEval(tensorflow.fmapicon_utils.do_nothing_model)
+        data.DavisEval(tensorflow_.fmapicon_utils.do_nothing_model, "do_nothing")
         
