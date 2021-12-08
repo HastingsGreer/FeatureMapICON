ad2c5ff5162f91ae128ad1a3cb63fc76e59922fd
Thomas Greer <tgreer@biag-gpu0.cs.unc.edu>
2021-12-08 16:39:43 -0500

    rip out all the trash

--- test/test_DAVIS.py
+++ test/test_DAVIS.py
@@ -1,11 +1,10 @@
 import unittest
 import data
-
-import tensorflow_.fmapicon_utils
+import fmapicon.utils
 
 class DavisTestCase(unittest.TestCase):
 #    def testDoNothing(self):
 #        data.DavisEval(tensorflow_.fmapicon_utils.do_nothing_model, "do_nothing")
     def testFMAPICON(self):
-        data.DavisEval(tensorflow_.fmapicon_utils.FMAPICON_model("tensorflow_/results/log_probability_2/epoch50/model_weights.tf"), "FMAPICON")
+        data.DavisEval(fmapicon.utils.FMAPICON_DAVIS_model("results/put_latest_model_here"), "FMAPICON")
         
--- test/test_imports.py
+++
@@ -1,6 +0,0 @@
-import unittest
-import tensorflow as tf
-
-class TFTestCase(unittest.TestCase):
-    def test_tf_gpu(self):
-        self.assertTrue(tf.test.is_gpu_available())
