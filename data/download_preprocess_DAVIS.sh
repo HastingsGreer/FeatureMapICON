dd0c805f2d62f17d2710d4b0aa5678bc809da822
Thomas Greer <tgreer@biag-gpu0.cs.unc.edu>
2021-12-08 20:41:50 -0500

    update dependencies

--- data/download_preprocess_DAVIS.sh
+++ data/download_preprocess_DAVIS.sh
@@ -3,8 +3,10 @@ cd data_storage
 
 if [ -f "~/.davis.zip" ]
     then
+        echo "used cached dataset"
         cp ~/.davis.zip DAVIS-2017-trainval-480p.zip
     else
+        echo "got new copy"
         wget -q https://data.vision.ee.ethz.ch/csergi/share/davis/DAVIS-2017-trainval-480p.zip 
         cp DAVIS-2017-trainval-480p.zip ~/.davis.zip
 fi
