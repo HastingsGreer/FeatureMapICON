a718222edace796047ee2bd4d33349949879e2a0
Thomas Greer <tgreer@biag-gpu0.cs.unc.edu>
2021-12-08 21:15:07 -0500

    last tweaks for unit test

--- test/test_DAVIS.py
+++ test/test_DAVIS.py
@@ -2,9 +2,15 @@ import unittest
 import fmapicon.davis_eval
 import fmapicon.utils
 
+import urllib.request
+from os.path import exists
 class DavisTestCase(unittest.TestCase):
 #    def testDoNothing(self):
 #        data.DavisEval(tensorflow_.fmapicon_utils.do_nothing_model, "do_nothing")
     def testFMAPICON(self):
-        fmapicon.davis_eval.DavisEval(fmapicon.utils.FmapICONSegmentationModel("results/no_crop_no_pretrain/network00019.trch"), "FMAPICON")
+        if not exists("network_00019.trch"):
+            print("Downloading pretrained model")
+            urllib.request.urlretrieve(
+              "https://github.com/uncbiag/FeatureMapICON/releases/download/tag/network00019.trch", "test/network00019.trch")
+        fmapicon.davis_eval.DavisEval(fmapicon.utils.FmapICONSegmentationModel("test/network00019.trch"), "FMAPICON")
         
