cd580e6608a1874a170c9cabc2bfc8112cea580b
Thomas Greer <tgreer@biag-gpu0.cs.unc.edu>
2021-12-10 17:30:57 -0500

    switch to kinetics

--- src/fmapicon/threaded_video_dataset.py
+++ src/fmapicon/threaded_video_dataset.py
@@ -10,7 +10,7 @@ import multiprocessing
 SHUFFLE_SIZE = 600
 
 BATCH_SIZE=64
-
+"""
 try:
     video_dir = "/playpen-nvme/tgreer/MannequinChallenge/train_512/"
     available_videos = os.listdir(video_dir)
@@ -25,6 +25,12 @@ except:
         video_dir = "/playpen-raid2/tgreer/Mannequin_Challenge/MannequinChallenge/train/"
         available_videos = os.listdir(video_dir)
         available_videos = [x for x in available_videos if ("mkv" in x) or ("mp4" in x)]
+"""
+
+import glob
+
+video_dir = "/playpen1/tgreer/Kinetics/kinetics_256/*/*.mp4"
+available_videos = glob.glob(video_dir)
 def framePacket():       
 
     max_gap = 30
@@ -32,7 +38,7 @@ def framePacket():
     # Create a VideoCapture object and read from input file
     fname = random.choice(available_videos)
 
-    cap = cv2.VideoCapture(video_dir + fname)
+    cap = cv2.VideoCapture(fname)
 
        
     # Check if camera opened successfully
@@ -40,7 +46,7 @@ def framePacket():
       print("Error opening video  file")
        
     length = cap.get(cv2.CAP_PROP_FRAME_COUNT) 
-    if length == 0:
+    if length < 64 + max_gap + 1:
         return framePacket()
     cap.set(cv2.CAP_PROP_POS_FRAMES, random.randint(0, length - 64 - max_gap - 1))
 
@@ -50,7 +56,7 @@ def framePacket():
             # try again.
             return framePacket()
         #if frame.shape != (512, 512, 3):
-        frame = frame[:480:4, :480:4] #cv2.resize(frame, (120, 120))
+        frame = frame[:240:2, :240:2] #cv2.resize(frame, (120, 120))
         images[i] = torch.tensor(frame).permute((2, 0, 1))
     output_index = (torch.arange(1, 65) + torch.randint(max_gap, (64,))).long() 
     x = torch.cat([images[:64], images[output_index]], 1)
@@ -80,7 +86,8 @@ def threadedProvide():
         packetProcesses[i] = multiprocessing.Process(target=putFramePacketsOnQueue, args=(packetQueue,), daemon=True)
         packetProcesses[i].start()
     shuffledQueue = multiprocessing.Queue(SHUFFLE_SIZE // 6)
-    cached_batches = torch.load("videocache64.pth")
+    #cached_batches = torch.load("videocache64.pth")
+    cached_batches = torch.load("kinetics_cache64.pth")
     process = multiprocessing.Process(target = grabShufflePut, args = (packetQueue, shuffledQueue), daemon=True)
     process.start()
     for c in cached_batches:
