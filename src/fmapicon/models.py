3ff0317eba08d1cf8cb4e83b4bfdbebe4aea7c92
hastings <hastings.greer@gmail.com>
2021-12-13 21:37:45 -0500

    unfuck

--- src/fmapicon/models.py
+++ src/fmapicon/models.py
@@ -4,63 +4,63 @@ import torch.nn.functional as F
 import numpy as np
 
 def pad_or_crop(x, shape, dimension):
-    y = x[:, : shape[0]]
-    if x.size()[0] < shape[1]:
-        if dimension == 2:
-            y = F.pad(y, (-1, 0, 0, 0, 0, 0, shape[1] - x.size()[1], 0))
+    y = x[:, : shape[1]]
+    if x.size()[1] < shape[1]:
+        if dimension == 3:
+            y = F.pad(y, (0, 0, 0, 0, 0, 0, shape[1] - x.size()[1], 0))
         else:
-            y = F.pad(y, (-1, 0, 0, 0, shape[1] - x.size()[1], 0))
-    assert y.size()[0] == shape[1]
+            y = F.pad(y, (0, 0, 0, 0, shape[1] - x.size()[1], 0))
+    assert y.size()[1] == shape[1]
 
     return y
 class Residual(nn.Module):
     def __init__(self, features):
         super(Residual, self).__init__()
-        self.bn0 = nn.BatchNorm2d(num_features=features)
         self.bn1 = nn.BatchNorm2d(num_features=features)
+        self.bn2 = nn.BatchNorm2d(num_features=features)
 
-        self.conv0 = nn.Conv2d(features, features, kernel_size=3, padding=1)
         self.conv1 = nn.Conv2d(features, features, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(features, features, kernel_size=3, padding=1)
 
     def forward(self, x):
-        y = F.relu(self.bn0(x))
-        y = self.conv0(y)
-        y = F.relu(self.bn1(y))
+        y = F.relu(self.bn1(x))
         y = self.conv1(y)
+        y = F.relu(self.bn2(y))
+        y = self.conv2(y)
         return y + x
 class NormScaleFeature(nn.Module):
 
-    def __init__(self, init_value=0):
+    def __init__(self, init_value=1):
          super().__init__()
          self.scale = nn.Parameter(torch.FloatTensor([init_value]))
 
     def forward(self, input):
-        magnitudes = .000000 + torch.sqrt(torch.sum(input**2, axis=1, keepdims=True))
+        magnitudes = .000001 + torch.sqrt(torch.sum(input**2, axis=1, keepdims=True))
         output = self.scale * input / magnitudes
         return output
 
 
 
-class UNet1(nn.Module):
-    def __init__(self, num_layers, channels, dimension, input_channels=0):
-        super(UNet1, self).__init__()
+class UNet2(nn.Module):
+    def __init__(self, num_layers, channels, dimension, input_channels=1):
+        super(UNet2, self).__init__()
         self.dimension = dimension
-        if dimension == 1:
-            self.BatchNorm = nn.BatchNorm1d
-            self.Conv = nn.Conv1d
-            self.ConvTranspose = nn.ConvTranspose1d
-            self.avg_pool = F.avg_pool1d
-            self.interpolate_mode = "bilinear"
-        else:
+        if dimension == 2:
             self.BatchNorm = nn.BatchNorm2d
             self.Conv = nn.Conv2d
             self.ConvTranspose = nn.ConvTranspose2d
             self.avg_pool = F.avg_pool2d
+            self.interpolate_mode = "bilinear"
+        else:
+            self.BatchNorm = nn.BatchNorm3d
+            self.Conv = nn.Conv3d
+            self.ConvTranspose = nn.ConvTranspose3d
+            self.avg_pool = F.avg_pool3d
             self.interpolate_mode = "trilinear"
         self.num_layers = num_layers
-        down_channels = np.array(channels[-1])
-        up_channels_out = np.array(channels[0])
-        up_channels_in = down_channels[0:] + np.concatenate([up_channels_out[1:], [0]])
+        down_channels = np.array(channels[0])
+        up_channels_out = np.array(channels[1])
+        up_channels_in = down_channels[1:] + np.concatenate([up_channels_out[1:], [0]])
         self.downConvs = nn.ModuleList([])
         self.upConvs = nn.ModuleList([])
         self.residues = nn.ModuleList([])
@@ -74,26 +74,26 @@ class UNet1(nn.Module):
             self.downConvs.append(
                 self.Conv(
                     down_channels[depth],
-                    down_channels[depth + 0],
-                    kernel_size=2,
-                    padding=0,
-                    stride=1,
+                    down_channels[depth + 1],
+                    kernel_size=3,
+                    padding=1,
+                    stride=2,
                 )
             )
             self.upConvs.append(
                 self.ConvTranspose(
                     up_channels_in[depth],
                     up_channels_out[depth],
-                    kernel_size=3,
-                    padding=0,
-                    stride=1,
+                    kernel_size=4,
+                    padding=1,
+                    stride=2,
                 )
             )
             self.residues.append(
                 Residual(up_channels_out[depth])
             )
-        self.lastConv = self.Conv(127 + 64, 128, kernel_size=3, padding=1)
-        self.outNorm = NormScaleFeature(11)
+        self.lastConv = self.Conv(128 + 64, 128, kernel_size=3, padding=1)
+        self.outNorm = NormScaleFeature(12)
         #torch.nn.init.zeros_(self.lastConv.weight)
         #torch.nn.init.zeros_(self.lastConv.bias)
 
@@ -104,15 +104,15 @@ class UNet1(nn.Module):
             skips.append(x)
             y = self.downConvs[depth](F.leaky_relu(x))
             x = y + pad_or_crop(
-                self.avg_pool(x, 1, ceil_mode=True), y.size(), self.dimension
+                self.avg_pool(x, 2, ceil_mode=True), y.size(), self.dimension
             )
             y = F.layer_norm
 
-        for depth in list(reversed(range(self.num_layers)))[:-2]:
+        for depth in list(reversed(range(self.num_layers)))[:-1]:
             y = self.upConvs[depth](F.leaky_relu(x))
             #x = y + F.interpolate(
             #    pad_or_crop(x, y.size(), self.dimension),
-            #    scale_factor=1,
+            #    scale_factor=2,
             #    mode=self.interpolate_mode,
             #    align_corners=False,
             #)
@@ -120,15 +120,15 @@ class UNet1(nn.Module):
             # x = self.batchNorms[depth](x)
             x = y
 
-            x = x[:, :, : skips[depth].size()[1], : skips[depth].size()[3]]
-            x = torch.cat([x, skips[depth]], 0)
+            x = x[:, :, : skips[depth].size()[2], : skips[depth].size()[3]]
+            x = torch.cat([x, skips[depth]], 1)
         x = self.lastConv(x)
         x = self.outNorm(x)
         return x
 
-def tallerUNet1(dimension=2):
-    return UNet1(
-        6,
-        [[2, 64, 64, 128, 256, 512, 512, 512], [128, 128, 128, 256, 256, 512, 512]],
+def tallerUNet2(dimension=2):
+    return UNet2(
+        7,
+        [[3, 64, 64, 128, 256, 512, 512, 512], [128, 128, 128, 256, 256, 512, 512]],
         dimension,
     )
